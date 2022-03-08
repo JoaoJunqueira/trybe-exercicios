79b5744ffef668ee4b311daf12f15aadbf8a83cf
JoaoJunqueira <vitjunqueira38@gmail.com>
2022-03-08 16:45:06 -0300

    Add comentário

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02/script1.js
@@ -0,0 +1,23 @@
+//https://app.betrybe.com/course/fundamentals/introducao-a-javascript-e-logica-de-programacao/javascript-array-e-loop-for/3d453622-6c99-46af-a884-fecc68811230/conteudos/7553db20-6428-4e93-8da7-8a71c7dec765/arrays-listas/9739ef35-84ca-4eb8-8818-c53b5d59dcdf?use_case=side_bar
+
+// Arrays
+// Exercício 1
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+// Exercício 2
+
+
+let indexOfPortfolio = menu.indexOf('Portfólio');
+console.log(indexOfPortfolio);
+
+// Exercício 3
+
+menu.push('Contato');
+console.log(menu);
+
+// For
+// Exercício 1
