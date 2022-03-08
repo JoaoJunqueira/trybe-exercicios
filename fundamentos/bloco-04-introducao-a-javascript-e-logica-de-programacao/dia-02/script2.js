fd531dc88869999d8f68f3c3ba2849563831d18b
JoaoJunqueira <vitjunqueira38@gmail.com>
2022-03-08 18:53:21 -0300

    Add até o exercício 6

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02/script2.js
@@ -0,0 +1,47 @@
+// Fonte: https://app.betrybe.com/course/fundamentals/introducao-a-javascript-e-logica-de-programacao/javascript-array-e-loop-for/3d453622-6c99-46af-a884-fecc68811230/exercicios/f93175b8-64ca-4d6a-b702-55fbbb579341/agora-a-pratica/4bfd26b3-7f08-4b26-863f-e4b32dde7e18?use_case=next_button
+
+// Exercício 1
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for(let i = 0; i < numbers.length; i++){
+    console.log(numbers[i]);
+}
+
+// Exercício 2
+
+resultado = 0;
+
+for(let i = 0; i < numbers.length; i++){
+    resultado = resultado + numbers[i];
+}
+
+console.log(resultado);
+
+// Exercício 3
+
+media = resultado/(numbers.length);
+console.log(media);
+
+// Exercício 4
+
+if(media > 20){
+    console.log("valor maior que 20");
+}
+else{
+    console.log("valor menor ou igual a 20");
+}
+
+// Exercício 5
+
+maior = 0;
+
+for(let i = 0; i < numbers.length; i++){
+    if(maior<numbers[i]){
+        maior = numbers[i];
+    }
+    else{
+
+    }
+}
+console.log(maior);
