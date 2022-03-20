2f748711a1f873a2de9c2f911c32ea61ad7f19ca
João Junqueira <99998751+JoaoJunqueira@users.noreply.github.com>
2022-03-20 15:43:21 -0300

    add comment

--- fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-01/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-01/script.js
@@ -1,3 +1,5 @@
+// https://app.betrybe.com/course/fundamentals/javascript-dom-eventos-e-web-storage/javascript-dom-e-seletores/d1494833-9ca9-4931-a2cb-75b860813446/exercicios/13b2addd-c6f0-46dd-be79-fc8865272027/funcoes-de-manipulacao-do-dom/518eea7a-1797-4386-ae7c-b3a65446daff?use_case=side_bar
+
 document.getElementsByClassName("main-content")[0].style.backgroundColor = "rgb(76,164,109)";
 document.getElementsByClassName("center-content")[0].style.backgroundColor = "white";
 document.getElementsByTagName("p")[0].innerText = "Num apartamento alugado";
