e2a5ecb4bc5d40394cfa5cb8178947be2942de4d
JoaoJunqueira <vitjunqueira38@gmail.com>
2022-06-08 11:03:34 -0300

    atualização 2 8/6/22

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04/script-exerc-2.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04/script-exerc-2.js
@@ -16,7 +16,35 @@ console.log(getPalindrom(string));
 
 // Exercicio 2 
 
-getIndex = (array) => {
+getHigherValueIndex = (array) => {
+    let array2 = []
+    for (let i = 0; i < array.length; i++) {
+        array2[i] = array[i];
+    }
+    let aux;
+    let index;
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = i; j < array.length; j++) {
+            if (array[i] > array[j]) {
+                aux = array[i];
+                array[i] = array[j];
+                array[j] = aux;
+            }
+        }
+    }
+    for (let i = 0; i < array.length; i++) {
+        if (array[array.length-1] === array2[i]) {
+            index = i;
+        }
+    }
+    return index;
+}
+
+console.log(getHigherValueIndex([2, 4, 6, 7, 10, 0, -3]));
+
+// Exercício 3
+
+getLowerValueIndex = (array) => {
     let array2 = []
     for (let i = 0; i < array.length; i++) {
         array2[i] = array[i];
@@ -40,5 +68,8 @@ getIndex = (array) => {
     return index;
 }
 
-console.log(getIndex([2, 4, 6, 7, 10, 0, -3]));
+console.log(getLowerValueIndex([2, 4, 6, 7, 10, 0, -3]));
+
+// Exercício 4
+
 
