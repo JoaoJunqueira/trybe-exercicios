049499befcc4239ffbad56bb975e824beb696d3b
JoaoJunqueira <vitjunqueira38@gmail.com>
2022-06-08 13:44:20 -0300

    requisito 3.0

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Requisito 3', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+  });
+  it(`Teste se é exibida na tela a mensagem No favorite pokemon found, 
+  caso a pessoa não tenha pokémons favoritos`, () => {
+    const favoriteLink = screen.getByText(/favorite pokémons/i);
+    userEvent.click(favoriteLink);
+
+    const message = screen.getByText(/no favorite pokemon found/i);
+    expect(message).toBeInTheDocument();
+  });
+  it('Teste se são exibidos todos os cards de pokémons favoritados.', () => {
+    const moreDetails = screen.getByText(/more details/i);
+    userEvent.click(moreDetails);
+
+    const favoritado = screen.getByRole('checkbox');
+    userEvent.click(favoritado);
+    // const favorito = screen.getAllByText(/pikachu/i);
+
+    const homeLink = screen.getByText(/home/i);
+    userEvent.click(homeLink);
+
+    const favoriteLink = screen.getByText(/favorite pokémons/i);
+    userEvent.click(favoriteLink);
+    const favorito = screen.getAllByText(/pikachu/i);
+
+    expect(favorito[0]).toBeInTheDocument();
+  });
+});
