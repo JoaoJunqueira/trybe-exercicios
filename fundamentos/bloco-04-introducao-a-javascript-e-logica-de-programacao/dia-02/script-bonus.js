c8c43e3750263bc733ae2bd047ce525cd0fe47b7
João Junqueira <99998751+JoaoJunqueira@users.noreply.github.com>
2022-06-28 19:16:20 -0300

    correção 28 jun

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02/script-bonus.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02/script-bonus.js
@@ -1,10 +1,6 @@
 // https://app.betrybe.com/course/fundamentals/introducao-a-javascript-e-logica-de-programacao/javascript-array-e-loop-for/3d453622-6c99-46af-a884-fecc68811230/exercicios/f93175b8-64ca-4d6a-b702-55fbbb579341/bonus/ac2ab0aa-3f26-48e7-818a-956b56ca3f97?use_case=next_button
 // http://devfuria.com.br/logica-de-programacao/introducao-ao-algoritmo-de-ordenacao-bubble-sort/
 
-// Exercício Bônus
-
-// Ordenar o array de forma crescente
-
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for(let i = 1; i < numbers.length; i += 1) {
@@ -19,7 +15,7 @@ for(let i = 1; i < numbers.length; i += 1) {
 
 console.log(numbers);
 
-// Ordenar o array de forma decrescente
+//
 
 for(let i = 1; i < numbers.length; i += 1) {
     for(let j = 0; j < i; j += 1) {
@@ -33,7 +29,7 @@ for(let i = 1; i < numbers.length; i += 1) {
 
 console.log(numbers);
 
-let array = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]; // Array original (apenas mudando o nome)
+let array = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let arrayNovo = [];
 
 for(let i = 0; i < array.length; i += 1){
