74824ce280cbf243571d6974410430684f1eb560
JoaoJunqueira <vitjunqueira38@gmail.com>
2022-07-05 08:44:19 -0300

    local change

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04/script-exerc-2.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04/script-exerc-2.js
@@ -72,4 +72,67 @@ console.log(getLowerValueIndex([2, 4, 6, 7, 10, 0, -3]));
 
 // Exercício 4
 
+const getLargerString = (array) => {
+    let aux;
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = i; j < array.length; j++) {
+            if (array[i].length > array[j].length) {
+                aux = array[i];
+                array[i] = array[j];
+                array[j] = aux;
+            }
+        }
+    }
+    return array[array.length - 1];
+}
+
+const array = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+
+console.log(getLargerString(array));
+
+// Exercício 5
+
+const findTheMoreRecurrent = (array) => {
+    let vector = []; // Vezes que o valor foi repetido
+    let vector1 = [];
+    for (let i = 0; i < array.length; i++) {
+        vector.push(0);
+    }
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[i] === array[j]) {
+                vector[i] += 1;
+            }
+        }
+    }
+    let aux;
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 1; j < array.length; j++) {
+            if (vector[i] >= vector[j]) {
+                aux = array[i];
+            }
+            return aux;
+        }
+    }
+    return aux;
+}
+
+const array1 = [2, 3, 2, 5, 8, 2, 3];
+
+console.log(findTheMoreRecurrent(array1));
+
+// Exercício 6
+
+const sum = (number) => {
+    let aux = 0;
+    for (let i = 1; i <= number; i++) {
+        aux += i;
+    }
+    return aux;
+}
+
+const number = 7;
+console.log(sum(number));
+
+// Exercício 7
 
