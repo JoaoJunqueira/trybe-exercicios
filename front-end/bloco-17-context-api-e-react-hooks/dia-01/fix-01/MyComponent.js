5ae3addec5fa544890fa4e1dfddc2f633b43ead3
JoaoJunqueira <vitjunqueira38@gmail.com>
2022-07-06 23:18:11 -0300

    modificações

---
+++ front-end/bloco-17-context-api-e-react-hooks/dia-01/fix-01/MyComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import MyContext from './MyContext';
+
+function MyComponent() {
+  return (
+    <MyContext.Provider value={123}>
+      <MyOtherComponent />
+    </MyContext.Provider>
+  )
+}
+
+export default MyComponent;
+
+//
---
+++ front-end/bloco-17-context-api-e-react-hooks/dia-01/fix-01/MyOtherComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import MyContext from './MyContext';
+
+function MyOtherComponent() {
+  return (
+    <MyContext.Consumer>
+      {(value) => (
+        <Something />
+      )}
+    </MyContext.Consumer>
+  )
+}
+
+export default MyOtherComponent;
+
