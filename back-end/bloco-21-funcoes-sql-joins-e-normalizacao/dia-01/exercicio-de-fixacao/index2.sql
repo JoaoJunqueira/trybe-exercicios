a53217993f75b4b3a4873d1640fc178931c7581d
JoaoJunqueira <vitjunqueira38@gmail.com>
2022-08-26 16:22:57 -0300

    comando 3: index2/ base de dados adicionado

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-01/exercicio-de-fixacao/index2.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-01/exercicio-de-fixacao/index2.sql
@@ -1,1 +1,3 @@
-SELECT title FROM sakila.film LIMIT 10;
+SELECT UCASE(title) FROM sakila.film LIMIT 10;
+SELECT LCASE(title) FROM sakila.film LIMIT 10;
+SELECT REPLACE(title, 'ACADEMY', 'FOO') FROM sakila.film WHERE film_id = 1;
