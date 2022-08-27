c8e44aa5b772571711a7028fa9faa6a827be4a85
JoaoJunqueira <vitjunqueira38@gmail.com>
2022-08-27 12:43:48 -0300

    index11.sql: criado e finalizado

---
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-01/exercicio-de-fixacao/index11.sql
@@ -0,0 +1,9 @@
+SELECT AVG(replacement_cost) FROM sakila.film;
+
+SELECT MIN(replacement_cost) FROM sakila.film;
+
+SELECT MAX(replacement_cost) FROM sakila.film;
+
+SELECT SUM(replacement_cost) FROM sakila.film;
+
+SELECT COUNT(replacement_cost) FROM sakila.film;
