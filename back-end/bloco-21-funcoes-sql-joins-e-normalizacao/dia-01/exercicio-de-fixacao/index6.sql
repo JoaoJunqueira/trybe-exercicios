32d6f7cac14129992c731b876d40ee30df2e80c1
JoaoJunqueira <vitjunqueira38@gmail.com>
2022-08-27 01:41:33 -0300

    index6: operacoes 3

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-01/exercicio-de-fixacao/index6.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-01/exercicio-de-fixacao/index6.sql
@@ -7,3 +7,8 @@ SELECT rental_duration + rental_rate FROM sakila.film LIMIT 10;
 SELECT rental_duration - rental_rate FROM sakila.film LIMIT 10;
 SELECT rental_duration / rental_rate FROM sakila.film LIMIT 10;
 SELECT rental_duration * rental_rate FROM sakila.film LIMIT 10;
+
+SELECT 10 DIV 3;
+SELECT 10 DIV 2;
+SELECT 14 DIV 3;
+SELECT 13 DIV 2;
