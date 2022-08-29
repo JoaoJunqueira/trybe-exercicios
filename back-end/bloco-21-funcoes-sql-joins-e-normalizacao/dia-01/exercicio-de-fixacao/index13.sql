e0620c0ded1aada066a72a05494c1d07ad7a599d
JoaoJunqueira <vitjunqueira38@gmail.com>
2022-08-29 16:42:40 -0300

    index13.sql add

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-01/exercicio-de-fixacao/index13.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-01/exercicio-de-fixacao/index13.sql
@@ -3,3 +3,11 @@ GROUP BY first_name;
 
 SELECT first_name, COUNT(*) FROM sakila.actor
 GROUP BY first_name;
+
+SELECT rating, AVG(length)
+FROM sakila.film
+GROUP BY rating;
+
+SELECT rating, MIN(replacement_cost)
+FROM sakila.film
+GROUP BY rating;
